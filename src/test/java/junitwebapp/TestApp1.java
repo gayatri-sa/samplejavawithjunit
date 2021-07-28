bd866565760e5ade24e4ac7894e9d2e1b2a66d74
Gayatri S Ajith <gayatri.sa@gmail.com>
2021-07-28 18:16:41 +0000

    Fixed the TestApp1 error I introduced before

--- src/test/java/junitwebapp/TestApp1.java
+++ src/test/java/junitwebapp/TestApp1.java
@@ -7,7 +7,7 @@ import junitwebapp.App;
 public class TestApp1 {
   @Test
   public void testPrintHelloWorld() {
-    Assert.assertEquals(App.getHelloWorld(), "Hello World1");
+    Assert.assertEquals(App.getHelloWorld(), "Hello World");
     
   }
 
